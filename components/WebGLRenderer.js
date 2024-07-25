57ddd59434b258c030e78339716bbc1e33549a04
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-07-24 21:29:42 -0500

    Update WebGLRenderer to render a rotating light triangular pyramid
    Update WebGLRenderer to render a rotating light triangular pyramid
    
    * Change vertex positions to define a triangular pyramid
    * Update indices to define a triangular pyramid
    * Modify shaders to work with the triangular pyramid
    * Adjust rotation logic to rotate the triangular pyramid
    * Change background color in global.css to white

--- components/WebGLRenderer.js
+++ components/WebGLRenderer.js
@@ -69,25 +69,21 @@ function initBuffers(gl) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   const positions = [
-    -1.0, -1.0,  1.0,
-     1.0, -1.0,  1.0,
-     1.0,  1.0,  1.0,
-    -1.0,  1.0,  1.0,
-    -1.0, -1.0, -1.0,
-     1.0, -1.0, -1.0,
-     1.0,  1.0, -1.0,
-    -1.0,  1.0, -1.0,
+    0.0,  1.0,  0.0,  // Top vertex
+   -1.0, -1.0,  1.0,  // Front-left vertex
+    1.0, -1.0,  1.0,  // Front-right vertex
+    1.0, -1.0, -1.0,  // Back-right vertex
+   -1.0, -1.0, -1.0,  // Back-left vertex
   ];
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   const faceColors = [
-    [0.0, 0.0, 0.0, 1.0],    // Front face: black
-    [0.0, 0.0, 0.0, 1.0],    // Back face: black
-    [0.0, 0.0, 0.0, 1.0],    // Top face: black
-    [0.0, 0.0, 0.0, 1.0],    // Bottom face: black
-    [0.0, 0.0, 0.0, 1.0],    // Right face: black
-    [0.0, 0.0, 0.0, 1.0],    // Left face: black
+    [0.75, 0.75, 0.75, 1.0],    // Front face: light grey
+    [0.75, 0.75, 0.75, 1.0],    // Right face: light grey
+    [0.75, 0.75, 0.75, 1.0],    // Back face: light grey
+    [0.75, 0.75, 0.75, 1.0],    // Left face: light grey
+    [0.75, 0.75, 0.75, 1.0],    // Bottom face: light grey
   ];
 
   var colors = [];
@@ -95,7 +91,7 @@ function initBuffers(gl) {
   for (var j = 0; j < faceColors.length; ++j) {
     const c = faceColors[j];
 
-    colors = colors.concat(c, c, c, c);
+    colors = colors.concat(c, c, c);
   }
 
   const colorBuffer = gl.createBuffer();
@@ -106,12 +102,12 @@ function initBuffers(gl) {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
   const indices = [
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    3,  2,  6,      3,  6,  7,    // top
-    4,  5,  1,      4,  1,  0,    // bottom
-    1,  5,  6,      1,  6,  2,    // right
-    4,  0,  3,      4,  3,  7,    // left
+    0,  1,  2,    // front face
+    0,  2,  3,    // right face
+    0,  3,  4,    // back face
+    0,  4,  1,    // left face
+    1,  2,  3,    // bottom face
+    1,  3,  4,    // bottom face
   ];
 
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
@@ -123,38 +119,28 @@ function initBuffers(gl) {
     // Front
      0.0,  0.0,  1.0,
      0.0,  0.0,  1.0,
-     0.0,  0.0,  1.0,
-     0.0,  0.0,  1.0,
+      0.0,  0.0,  1.0,
+
+    // Right
+     1.0,  0.0,  0.0,
+     1.0,  0.0,  0.0,
+     1.0,  0.0,  0.0,
 
     // Back
      0.0,  0.0, -1.0,
      0.0,  0.0, -1.0,
      0.0,  0.0, -1.0,
-     0.0,  0.0, -1.0,
 
-    // Top
-     0.0,  1.0,  0.0,
-     0.0,  1.0,  0.0,
-     0.0,  1.0,  0.0,
-     0.0,  1.0,  0.0,
+    // Left
+    -1.0,  0.0,  0.0,
+    -1.0,  0.0,  0.0,
+    -1.0,  0.0,  0.0,
 
     // Bottom
      0.0, -1.0,  0.0,
      0.0, -1.0,  0.0,
      0.0, -1.0,  0.0,
      0.0, -1.0,  0.0,
-
-    // Right
-     1.0,  0.0,  0.0,
-     1.0,  0.0,  0.0,
-     1.0,  0.0,  0.0,
-     1.0,  0.0,  0.0,
-
-    // Left
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0
   ];
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
@@ -186,8 +172,8 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
   const modelViewMatrix = mat4.create();
 
   mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
-  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
-  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, pyramidRotation, [0, 1, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, pyramidRotation * .7, [1, 0, 0]);
 
   const normalMatrix = mat4.create();
   mat4.invert(normalMatrix, modelViewMatrix);
@@ -265,16 +251,16 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     normalMatrix);
 
   {
-    const vertexCount = 36;
+    const vertexCount = 18;
     const type = gl.UNSIGNED_SHORT;
     const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
   }
 
-  cubeRotation += deltaTime;
+  pyramidRotation += deltaTime;
 }
 
-let cubeRotation = 0.0;
+let pyramidRotation = 0.0;
 
 function WebGLRenderer() {
   const canvasRef = useRef(null);
